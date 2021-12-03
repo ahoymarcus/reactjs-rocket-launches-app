108b25399ca923401f0054f8f5bfa05dc419a1bb
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-03 12:53:18 -0300

    Definir estrutura da pagina Login. Estilizar pagina Login.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,48 @@
 import React from 'react';
 
+import './login.css';
+
 
 
 const Login = () => {
 	
 	
 	return (
-		<div>
-			Login
-		</div>
+		<div className="login-container">
+			<header className="login-header">
+				<h1>Rocket-X Launches</h1>
+				<p>Cadastre-se aqui para poder acompanhar essa saga interestelar!</p>
+				
+			</header>
+			<main className="login-main">
+				<section className="login-info">
+					<h2>Cadastre usuário</h2>
+					<form className="login-form" >
+						<div className="input-container">
+							<label htmlFor="username">
+								Usuário
+							</label>
+							<input 
+								type="text"
+								id="username"
+								placeholder="nome do usuário"
+							/>
+						</div>
+						<div className="input-container">
+							<label htmlFor="password">
+								Senha
+							</label>
+							<input
+								type="text"
+								id="password"
+								placeholder="sua senha"
+							/>
+						</div>
+					</form>
+				</section>
+				
+			</main>
+	</div>
 	);
 };
 
