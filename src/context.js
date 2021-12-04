ed8901022baeddfea680f51aa9e46221f8cfa7fe
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-03 22:28:44 -0300

    Iniciar construcao de fetchLoginToken em context.js

--- src/context.js
+++ src/context.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect, useCallback } from 'react';
 
 
-const url = 'http://localhost:3000';
+const loginUrl = 'http://localhost:3000/api/v1/login';
 
 
 const AppContext = React.createContext();
@@ -10,6 +10,28 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
 	const [ username, setUsername ] = useState('');
 	const [ password, setPassword ] = useState('');
+	
+	// https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
+	const fetchLoinToken = async () => {
+		try {
+			const response = await fetch(loginUrl);
+			const data = await response.json();
+			
+			console.log(data);
+			// const token = data.
+			
+			
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	
+	
+	useEffect(() => {
+		if (username !== '' && password !== '') {
+			fetchLoinToken();
+		}
+	}, [username, password]);
 	
 	
 	
